a22244650a848d377725f130881f0005d169a707
Juan <jguardado85@gmail.com>
2016-05-04 16:51:04 -0700

    adding new challenge

---
+++ noName-Karun2.js
@@ -0,0 +1,5 @@
+// Write a program to determine the answer to the following question.
+
+// A farmer wants to spend exactly $100 to buy exactly 100 animals.
+// Cows cost $10, pigs cost $3 and chickens cost 50 cents.
+// He must buy at least one of each. How may of each type of animal should he buy?
